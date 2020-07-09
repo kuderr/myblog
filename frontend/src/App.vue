98a71b2b9036499a27dffcaf76ef2b073faa172a
kuderr <dakudryavcev@gmail.com>
2020-07-10 02:05:59 +0300

    avatar and soc added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,75 @@
 <template>
   <div id="app">
-    <v-content>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
+    <div class="demo-type">
+      <el-avatar :size="125">
+        <img
+          src="https://avatars0.githubusercontent.com/u/39552217?s=460&u=415aebf4249492578b8b2af663f5e5473c704dd4&v=4"
+        />
+      </el-avatar>
+    </div>
+    <div>
+      <el-link
+        v-for="soc in social"
+        :key="soc.url"
+        :href="soc.url"
+        target="_blank"
+      >
+        <i :class="soc.icon"></i>
+      </el-link>
+    </div>
+    <el-menu
+      :default-active="curPath"
+      class="el-menu"
+      mode="horizontal"
+      @select="handleSelect"
+      :router="true"
+    >
+      <el-menu-item index="/">Главная</el-menu-item>
+      <el-menu-item index="/posts">Посты</el-menu-item>
+      <el-menu-item index="/about">Об авторе</el-menu-item>
+    </el-menu>
+
+    <el-container>
+      <el-main> <router-view></router-view> </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      curPath: localStorage.curPath || "/",
+      social: [
+        {
+          url: "https://github.com/kuderr",
+          icon: "el-icon-fa-github",
+        },
+        {
+          url: "https://twitter.com/kuderrr",
+          icon: "el-icon-fa-twitter",
+        },
+        {
+          url: "https://vk.com/kuderrr",
+          icon: "el-icon-fa-vk",
+        },
+        {
+          url: "https://www.instagram.com/kuderr_/",
+          icon: "el-icon-fa-instagram",
+        },
+        {
+          url: "mailto:dakudryavcev@gmail.com",
+          icon: "el-icon-fa-envelope",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      localStorage.curPath = key;
+    },
+  },
 };
 </script>
 
@@ -21,6 +80,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 60%;
+  margin: 30px auto;
+}
+
+i {
+  margin: 0 10px;
 }
 </style>
