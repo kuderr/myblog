f74b6b3b8c70c9ad7fc3d30d16f7c171ed064e36
kuderr <dakudryavcev@gmail.com>
2020-07-19 16:31:28 +0300

    new editor component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import App from "@/App.vue";
 import vuetify from "@/plugins/vuetify";
+import { TiptapVuetifyPlugin } from "tiptap-vuetify";
+import "tiptap-vuetify/dist/main.css";
 import router from "@/router";
 import store from "@/store";
 
 Vue.config.productionTip = false;
+Vue.use(TiptapVuetifyPlugin, {
+  vuetify,
+});
 
 new Vue({
   vuetify,
