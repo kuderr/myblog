b907ebe1065385a763fb5ce606e4bca913be774a
kuderr <dakudryavcev@gmail.com>
2020-08-15 23:33:23 +0300

    fuck it

--- nuxtfront/store/user/index.ts
+++ nuxtfront/store/user/index.ts
@@ -7,11 +7,10 @@ import {
   updatePublishedStatus,
 } from '@/api'
 import { User } from './models'
-import { Post } from '..posts/models'
 
 export default {
   state: {
-    userPosts: Array<Post>(),
+    userPosts: Array<any>(),
     user: new User(),
     token: '',
   },
@@ -28,15 +27,15 @@ export default {
       state.token = null
       localStorage.removeItem('token')
     },
-    userPostsLoaded(state, payload: Post[]) {
+    userPostsLoaded(state, payload) {
       state.userPosts = payload
     },
-    userPostAdded(state, payload: Post) {
+    userPostAdded(state, payload) {
       state.userPosts.unshift(payload)
     },
     userPostDeleted(state, postId: number) {
       let i: number
-      state.userPosts.forEach((post: Post, index: number) => {
+      state.userPosts.forEach((post, index: number) => {
         if (post.id === postId) {
           i = index
           return
@@ -44,8 +43,8 @@ export default {
       })
       state.userPosts.splice(i, 1)
     },
-    userPostUpdated(state, payload: Post) {
-      state.userPosts.forEach((post: Post, index: number) => {
+    userPostUpdated(state, payload) {
+      state.userPosts.forEach((post, index: number) => {
         if (post.id === payload.id) {
           state.userPosts[index] = payload
           return
@@ -99,23 +98,23 @@ export default {
         commit('setError', error)
       }
     },
-    async addPost({ state, commit }, postData: Post) {
+    async addPost({ state, commit }, postData) {
       let res = await addPost(postData, state.token)
       postData.id = res.data.postId
       commit('userPostAdded', postData)
       return res
     },
-    updatePublishedStatus({ state, commit }, post: Post) {
+    updatePublishedStatus({ state, commit }, post) {
       updatePublishedStatus(post.id, post.published, state.token)
       if (post.published === false) commit('postDeleted', post.id)
       else commit('postAdded', post)
     },
-    deletePost({ state, commit }, post: Post) {
+    deletePost({ state, commit }, post) {
       deletePost(post.id, state.token)
       commit('userPostDeleted', post.id)
       if (post.published === true) commit('postDeleted', post.id)
     },
-    updatePost({ state, commit }, post: Post) {
+    updatePost({ state, commit }, post) {
       updatePost(post, state.token)
       commit('userPostUpdated', post)
       if (post.published === true) commit('postUpdated', post)
